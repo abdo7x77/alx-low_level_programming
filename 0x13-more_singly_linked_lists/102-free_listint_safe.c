d5923a89b3c7048b24ed5de118ffb7b2532ae04c
abdo <abdelrhman1562004@gmail.com>
2023-04-29 13:48:34 -0700

    0

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,51 +1,80 @@
 #include "lists.h"
+#include <stdio.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
+ /**
+  * looped_listint_len - Counts the number of unique nodes
+  *                      in a looped listint_t linked list.
+  * @head: A pointer to the head of the listint_t to check.
+  *
+  * Return: If the list is not looped - 0.
+  *         Otherwise - the number of unique nodes in the list.
+  */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+	return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				tortoise = tortoise->next;
+				while (tortoise != hare)
+				{
+					nodes++;
+					tortoise = tortoise->next;
+				}
+				return (nodes);
+			}
+			tortoise = tortoise->next;
+			hare = (hare->next)->next;
+		}
+
+		return (0);
+	}
 
 /**
-* free_listint_safe - frees a list
-* @h: the pointer to a pointer of head
-*
-* Description: does stuff. i didnt get this right
-*	this is similar to the last problem, but now we
-*	want to free instead of print. inside the second loop
-*	pretty much the same code to iterate through, just free it all
-* Return: the size of the list
-*/
-
-size_t free_listint_safe(listint_t **h)
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	listint_t **array;
-	unsigned int i = 0;
-	unsigned int flag = 0;
+	size_t nodes, index = 0;
 
-	array = malloc(sizeof(listint_t *) * 1024);
-	if (!array)
-		exit(98);
-	while (*h != NULL)
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
 	{
-		for (i = 0; i < count; i++)
+		for (; head != NULL; nodes++)
 		{
-			if (*h == array[i])
-			{
-				flag = 1;
-				break;
-			}
-			else
-				flag = 0;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-		if (flag == 1)
-			break;
-		array[count] = *h;
-		*h = (*h)->next;
-		count++;
 	}
-	i = 0;
-	while (i < count)
+	else
 	{
-		free(array[i]);
-		i++;
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
-	free(array);
-	*h = NULL;
-	return (count);
+	return (nodes);
 }
